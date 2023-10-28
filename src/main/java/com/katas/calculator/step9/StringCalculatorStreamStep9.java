eba72bce7def107fb3675aba271de2bcd5ac4775
RR <rr@phoebussoftware.com>
2023-10-28 13:52:20 +0100

    Step-9: Add step 9 class and test

---
+++ src/main/java/com/katas/calculator/step9/StringCalculatorStreamStep9.java
@@ -0,0 +1,11 @@
+package com.katas.calculator.step9;
+
+import com.katas.calculator.Calculator;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class StringCalculatorStreamStep9 implements Calculator {
+    @Override
+    public int add(String numbers) {
+        throw new NotImplementedException();
+    }
+}
