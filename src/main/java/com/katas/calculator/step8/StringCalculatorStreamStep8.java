a22d470011f8d47a3b4e15284dbc356b9e82904d
RR <rr@phoebussoftware.com>
2023-11-04 11:27:40 +0000

    Step-8v2: Organize packages, add dependencies, non-stream solution and tests

---
+++ src/main/java/com/katas/calculator/step8/StringCalculatorStreamStep8.java
@@ -0,0 +1,11 @@
+package com.katas.calculator.step8;
+
+import com.katas.calculator.Calculator;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class StringCalculatorStreamStep8 implements Calculator {
+    @Override
+    public int add(String input) {
+        throw new NotImplementedException();
+    }
+}
